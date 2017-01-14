1ecbad4a352f2c21255ab53a04bebe19b06afd5c
Marc Anders <marcsapps1@gmail.com>
2017-01-13 23:31:13 -0600

    New Express Gernerator using Jade Mongo Monk

---
+++ nodetest1/app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();      //-+ This instatiates express, assigns our app variable to it...
+
+//-=--------------------------------------------------------------------------------------------------This stuff tess app where to find views, engine to render with (jade), and some methods
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));//-=--------------------------------------------Views rewrite url to top path \
+app.set('view engine', 'jade');//-=-------------------------------------------------------------------------------------------------- use jade to render
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));//-=--------------------------------------------Stattic pages from here (/public) but rewrite url to top path  
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;//-=--------------------------------------------Master app eports its app object to be called (core node)  
